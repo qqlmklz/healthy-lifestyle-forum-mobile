69353101fd7af4de10215c5c1fd82a00fca34498
qqlmklz <qqlmklz@gmail.com>
2025-01-19 16:22:45 +0300

    fix: refactor burger menu

--- components/LayoutWithMenu/index.tsx
+++ components/LayoutWithMenu/index.tsx
@@ -1,18 +1,12 @@
-import { RootState } from '@/app/store';
 import { BurgerMenu } from '@/components/BurgerMenu';
-import { useAppSelector } from '@/shared/hooks/useAppSelector';
 import React, { FC, PropsWithChildren } from 'react';
 import { View } from 'react-native';
 
-export const LayoutWithMenu: FC<PropsWithChildren> = ({ children }) => {
-  const menuOpen = useAppSelector((state: RootState) => state.burgerMenu);
-
-  return (
-    <>
-      {/* Боковое меню */}
-      <BurgerMenu />
-      {/* Контент страницы */}
-      <View className="flex-1 bg-white mt-[-850px]">{children}</View>
-    </>
-  );
-};
+export const LayoutWithMenu: FC<PropsWithChildren> = ({ children }) => (
+  <>
+    {/* Боковое меню */}
+    <BurgerMenu />
+    {/* Контент страницы */}
+    <View className="flex-1 bg-white mt-[-850px]">{children}</View>
+  </>
+);
